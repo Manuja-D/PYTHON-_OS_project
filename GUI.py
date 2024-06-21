76d2f7a38095551bb9806f120d9ea05bd0041e6e
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-06-21 23:28:57 +0530

    note taking app faills

--- GUI.py
+++ GUI.py
@@ -4,8 +4,19 @@ from tkinter import ttk
 #A simple window
 window = tk.Tk()
 window.title('demo')
-window.geometry('300x150')
+window.geometry('400x300')
 
 #gui
 
 window.mainloop()
+
+note_entry = tk.Text(window)
+note_entry.pack()
+
+import sqlite3
+
+def save():
+    note = note_entry.get("1.0",tk.END)
+    conn = sqlite3.conect("notes.db")
+    cursor = conn.cursor()
+    
