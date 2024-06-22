ecff42f5cf4c5a897e47285fcfa4b74f88582b2a
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-06-22 23:34:00 +0530

    note taking terminal update

--- Note_taking.py
+++ Note_taking.py
@@ -10,9 +10,18 @@ y_or_n = input("do you need to save this file? (y or n) :")
 if(y_or_n == "y" or y_or_n == "Y"):
     print(i)
     permenent_note = i
+    print("Youre note has been added to permentent note")
 
 else:
     print("this msg will be deleted after you turn off the machine \n")
     print(i)
 
-print(permenent_note)
+print("We have your permenent notes collected do you want to see them?")
+print("(Y OR N)")
+
+ans = input()
+
+if (ans == "y" or ans == "Y"):
+    print(permenent_note)
+else:
+    print("Nothing is saved :} ")
